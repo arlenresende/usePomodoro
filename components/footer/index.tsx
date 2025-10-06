2f349a8de890a686813ccdef0e8556109d62c6ca
Arlen Resende <arlen.resende@gmail.com>
2025-10-06 11:32:46 -0300

    docs: update copyright year in footer to 2025

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <div className="text-gray-500 text-sm lg:text-base">
-      usepomodoro@2024.
+      usepomodoro@2025.
       <br className="block lg:hidden" />
       All rights reserved.
     </div>
